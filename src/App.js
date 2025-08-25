9d07a25bd5fa3012d5f45121e3ef073d1e18596c
eusouleony <eusouleony@gmail.com>
2025-08-25 15:30:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -539,7 +539,6 @@ function App() {
                 }
               />
             </div>
-
             <button
               className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
               onClick={adicionarServico}
